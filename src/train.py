0ce163e52ac464eff0a825706b69f5bef52935ae
Lucas Yan <112940489+LubabDesu@users.noreply.github.com>
2025-09-15 22:42:57 -0700

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,5 +1,5 @@
 # Training loop
-from model import PianoReductionTransformer, 
+from model import PianoReductionTransformer
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -10,7 +10,12 @@ from onsetsandframes import OnsetsAndFrames
 def train_model(model, train_loader, val_loader, num_epochs = 100, device='cpu') :
     pos_weight_value = 0.4
     optimizer = optim.Adam(model.parameters(), lr = 0.001)
-    scheduler = StepLR(optimizer, step_size=5, gamma=0.1)
+    # scheduler = StepLR(optimizer, step_size=5, gamma=0.1)
+    scheduler = torch.optim.lr_scheduler.OneCycleLR(
+        optimizer, max_lr=1e-3,
+        steps_per_epoch=len(train_loader),
+        epochs=num_epochs, pct_start=0.1
+    )
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print("Current device being used is .... : " + str(device))
     model = model.to(device)
@@ -19,8 +24,10 @@ def train_model(model, train_loader, val_loader, num_epochs = 100, device='cpu')
     for epoch in range(num_epochs):
         model.train()  # Set model to training mode
         running_loss = 0.0
- 
-        for batch in train_loader:
+        
+        for batch_idx, batch in enumerate(train_loader):
+            # print(f"  → Epoch {epoch+1}, Batch {batch_idx+1}/{len(train_loader)}")
+            # print("im in the batch for loop\n")
             for key in batch: # Move the tensors to the correct device
                 if isinstance(batch[key], torch.Tensor):
                     batch[key] = batch[key].to(device)
@@ -30,16 +37,32 @@ def train_model(model, train_loader, val_loader, num_epochs = 100, device='cpu')
             # Forward pass
             optimizer.zero_grad()  # Clear previous gradients
             predictions = model(batch_inputs)  # Get predictions
-            losses = loss_calc.onsetFrameLoss(batch, predictions=predictions)
-            loss = sum(losses.values())
+
+            # losses = loss_calc.onsetFrameLoss(batch, predictions=predictions)
+            # loss_onset = losses['loss/onset']
+            # loss_offset = losses['loss/offset']
+            # loss_frame = losses['loss/frame']
+            # loss = (18 * loss_onset + loss_offset + loss_frame) / 20
+            # print("Calculated losses!!!!!!\n")
+            loss = loss_calc.onsetFrameLoss(batch, predictions=predictions)
 
             # Backward pass and optimize
+            # print("Starting backward...\n")
             loss.backward()  # Compute gradients
+            # print("Backward done!\n")
+            
             optimizer.step()  # Update weights
+            scheduler.step()
+            optimizer.zero_grad(set_to_none=True)
 
             running_loss += loss.item()
 
+            # print(f"  → Epoch {epoch+1}, Current loss : {running_loss}")
+            
+
+
         epoch_loss = running_loss / len(train_loader)
+        # print(f"  → Epoch {epoch+1}, Current loss : {epoch_loss}")
 
         #Validation phase
         model.eval() #Set model to evaluation mode 
@@ -54,8 +77,13 @@ def train_model(model, train_loader, val_loader, num_epochs = 100, device='cpu')
                 batch_targets = batch['target']
 
                 outputs = model(batch_inputs)
-                val_losses = loss_calc.onsetFrameLoss(outputs, batch_targets)
-                val_loss = sum(val_losses.values())
+                # val_losses = loss_calc.onsetFrameLoss(batch, predictions=outputs)
+                # val_loss_onset = val_losses['loss/onset']
+                # val_loss_offset = val_losses['loss/offset']
+                # val_loss_frame = val_losses['loss/frame']
+                # val_loss = (18 * val_loss_onset + val_loss_offset + val_loss_frame) / 20
+                val_loss = loss_calc.onsetFrameLoss(batch, predictions=outputs)
+                # val_loss = sum(val_losses.values())
                 running_val_loss += val_loss.item()
 
         epoch_val_loss = running_val_loss / len(val_loader)
